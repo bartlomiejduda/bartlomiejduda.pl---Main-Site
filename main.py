8e3627cc5f6dd31cf60b8dbec8f86ec97700fad2
Bartlomiej Duda <ikskoks@gmail.com>
2021-03-25 00:31:25 +0100

    Main Site update 25.03.2021 00:31

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
+import random
 
 app = Flask(__name__)
 
 @app.route("/")
 def homepage():
-    return render_template("index.html", title="HOME PAGE")
+    rand_seed = random.randint(0, 100000)
+    return render_template("index.html", title="HOME PAGE", seed=rand_seed)
 
 if __name__ == "__main__":
     app.run()
